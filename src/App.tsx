a48472a6e15ca102982f98164c4b03ceab82a4c9
aca-ta <seljydef@gmail.com>
2021-03-07 15:34:08 +0900

    update: add and use prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './App.css'
-import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 //船橋市のlat lon
-const position:[number, number] = [35.694722, 139.9825]
+const position: [number, number] = [35.694722, 139.9825];
 const App = () => {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '100vh' }}>
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "100vh" }}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -17,6 +22,6 @@ const App = () => {
         </Popup>
       </Marker>
     </MapContainer>
-  )
-}
-export default App
+  );
+};
+export default App;
