d6cfb9daa50691e57432b02892adbc1f09493c1a
aca-ta <seljydef@gmail.com>
2021-03-07 14:39:05 +0900

    fix.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css'
+import 'leaflet/dist/leaflet.css'
 
-const position = [51.505, -0.09]
+//船橋市のlat lon
+const position = [35.694722, 139.9825]
 const App = () => {
   return (
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-)
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '100vh' }}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
 }
 export default App
