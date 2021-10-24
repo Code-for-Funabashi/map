5d5c1f1d217a2f9f9f0ebaf97fdb35f04f0b1b55
kyoya0819 <kyoya0819@gmail.com>
2021-10-24 14:16:36 +0900

    ファイルの移動

--- src/components/Map/Navigation/Navigation.tsx
+++ src/components/Map/Navigation/Navigation.tsx
@@ -1,101 +1,14 @@
-import React, {
-  FC,
-  useState,
-  useEffect,
-  ChangeEvent,
-  Dispatch,
-  SetStateAction,
-} from "react";
-import { useMap } from "react-leaflet";
-import L, { Marker, LayerGroup } from "leaflet";
-import axios from "axios";
+import React, { FC } from "react";
 
-import { PointInfo, PointMeta } from "types/Point";
+import Layers from "components/Map/Layers";
 
 import scss from "./Navigation.module.scss";
 
-import { points } from "data/points/points";
-
 const Navigation: FC = () => {
-  const map = useMap();
-  const [layerGroups, setLayerGroups] = useState<{ [key: string]: LayerGroup }>(
-    {}
-  );
-  const [layerGroupKeys, setLayerGroupKeys] = useState<{
-    [key: string]: boolean;
-  }>({});
-
-  useEffect(() => {
-    Promise.all(
-      points().map((point: PointMeta) => {
-        return axios.get<[PointInfo]>(point.url);
-      })
-    ).then((responses) => {
-      const types: { [key: string]: boolean } = {};
-      const groups: { [key: string]: LayerGroup } = {};
-      responses.forEach((response, index) => {
-        const type = points()[index].type;
-        types[type] = true;
-
-        const markers: Marker[] = [];
-        response.data.forEach((marker: PointInfo) => {
-          const name = marker.name;
-          const address = marker.details.address;
-          const phone = marker.details.phone_number;
-          const HTML = `種別: ${type}<br />名前: ${name}<br />住所: ${address}<br />電話番号: ${phone}`;
-
-          markers.push(
-            L.marker([marker.lat, marker.lng], {
-              icon: points()[index].icon,
-            }).bindPopup(HTML)
-          );
-        });
-
-        groups[type] = L.layerGroup(markers).addTo(map);
-      });
-
-      setLayerGroupKeys(types);
-      setLayerGroups(groups);
-    });
-  }, [map]);
-
-  useEffect(() => {
-    Object.keys(layerGroupKeys).forEach((key) => {
-      if (layerGroups[key] !== undefined) {
-        map.removeLayer(layerGroups[key]);
-        if (layerGroupKeys[key]) map.addLayer(layerGroups[key]);
-      }
-    });
-  }, [map, layerGroupKeys, layerGroups]);
-
   return (
     <div className={scss.navigation}>
-      {Object.keys(layerGroups).map((key, index) => {
-        return (
-          <label key={index}>
-            {key}
-            <input
-              type="checkbox"
-              value={key}
-              checked={layerGroupKeys[key] ?? true}
-              onChange={(e) => checkBoxClickHandler(e, setLayerGroupKeys)}
-            />
-          </label>
-        );
-      })}
+      <Layers />
     </div>
   );
 };
 export default Navigation;
-
-const checkBoxClickHandler = (
-  e: ChangeEvent<HTMLInputElement>,
-  setActiveKeys: Dispatch<SetStateAction<{ [key: string]: boolean }>>
-) => {
-  setActiveKeys((prevState) => {
-    return {
-      ...prevState,
-      [e.target.value]: e.target.checked,
-    };
-  });
-};
