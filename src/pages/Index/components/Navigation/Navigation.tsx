cff479700d6997fdb25752fa53fe050534c0c684
kyoya0819 <kyoya0819@gmail.com>
2021-10-24 15:02:43 +0900

    短期間でクリックするとマップが拡大してしまう問題の修正

--- src/pages/Index/components/Navigation/Navigation.tsx
+++ src/pages/Index/components/Navigation/Navigation.tsx
@@ -1,13 +1,19 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import L from "leaflet";
 
 import Layers from "pages/Index/components/Layers";
 
 import scss from "./Navigation.module.scss";
 
 const Navigation: FC = () => {
+  useEffect(() => {
+    L.DomEvent.disableClickPropagation(
+      document.getElementsByClassName(scss.navigation)[0] as HTMLElement
+    );
+  }, []);
+
   return (
     <div className={scss.navigation}>
-      <Layers />
       <div className={scss.layers}>
         <Layers />
       </div>
