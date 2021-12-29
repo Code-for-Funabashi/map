b392cdcd5f1a4888f18037743f5c5b4c1b57005f
j-takurou <nyk523jt@gmail.com>
2021-12-29 23:03:56 +0900

    update indent

--- src/types/Polygon.ts
+++ src/types/Polygon.ts
@@ -1,15 +1,15 @@
 import { PathOptions, LatLngExpression } from "leaflet";
 
 export interface PolygonMeta {
-    type: string;
-    url: string;
-    option: PathOptions;
+  type: string;
+  url: string;
+  option: PathOptions;
   }
 
 
 
 export interface PolygonInfo {
-    type: any;
-    name: string;
-    coordinates: LatLngExpression[][][];
+  type: any;
+  name: string;
+  coordinates: LatLngExpression[][][];
   }
